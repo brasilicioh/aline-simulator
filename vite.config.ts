966cd903025345d4c5b9b0d19df14b69d700b679
brasilicioh <brasiliciohcr@gmail.com>
2026-06-22 09:49:48 -0300

    misc: add paths to project's config to use alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/aline-simulator/",
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@pages": resolve(__dirname, "src/pages"),
+      "@mru": resolve(__dirname, "src/simulations/mru"),
+      "@charts": resolve(__dirname, "src/components/charts"),
+      "@animation": resolve(__dirname, "src/hooks/useAnimation.ts"),
+      "@assets": resolve(__dirname, "./src/assets"),
+    },
+  },
 });
